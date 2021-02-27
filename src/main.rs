36a3cb9eec3474d131942f06cdca5bf3d979fca7
João Salomão <joaosalomao.613@gmail.com>
2021-02-27 20:39:43 -0300

    Corrigindo mensagem de erro

--- src/main.rs
+++ src/main.rs
@@ -170,6 +170,6 @@ fn get_input() -> String {
     let mut input = String::new();
     io::stdin()
         .read_line(&mut input)
-        .expect("Error getting the player one name");
+        .expect("Error getting user input");
     input.replace("\r\n", "")
 }
