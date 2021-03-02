f6aeac841ccb3cdbee7673fbdb71b7f5fcc41264
João Salomão <joaosalomao.613@gmail.com>
2021-03-02 01:07:28 -0300

    Testando módulo player

--- src/core/player.rs
+++ src/core/player.rs
@@ -12,3 +12,16 @@ impl Player {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_return_player_marker() {
+        assert_eq!("X", Player::One.get_marker());
+        assert_eq!("O", Player::Two.get_marker());
+        assert!("C" != Player::One.get_marker());
+        assert!("V" != Player::Two.get_marker());
+    }
+}
